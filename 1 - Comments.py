1b0b4a70ea940555db4040a4a5214e8267e2aa5d
Gabriel B. Scholl <48660276+GabrielScholl@users.noreply.github.com>
2022-04-08 18:14:38 -0300

    Added the firt files

---
+++ 1 - Comments.py
@@ -0,0 +1,20 @@
+# This file goes on to show the different types of comments in Python
+
+# This is a comment
+
+print('Hi') # This is also a comment, but at the end of a line
+
+'''This is a different type of comment, 
+that can be multiple lines long.'''
+
+"""
+Double qotes can also be used for this purpose.
+And neither have to be at the same line as the comments,
+Just before and after.
+"""
+
+def more(code):
+    # print("I do nothing because I'm commented")
+    return
+
+# And that about covers comments in Python
---
+++ README.txt
@@ -0,0 +1,2 @@
+Olá! Este repositório tem como objetivo demonstrar o uso de todas as estruturas de Python, além de alguns módulos e bibliotecas.
+Cada arquivo será dedicado a um tópico listado na página Python do site https://www.w3schools.com/python/.

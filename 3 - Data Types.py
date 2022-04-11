6f4069192a2013521bc8cb99e7f935c682d9d9dd
GabrielScholl <boaventura063@gmail.com>
2022-04-11 11:11:01 -0300

    commit

--- 3 - Data Types.py
+++ 3 - Data Types.py
@@ -3,5 +3,35 @@
 # Text, numbers, sequences, mapping, sets, boolean or binary.
 # Each one has different uses and constructions, as we'll see below.
 
-# Numbers
+# Numbers ------------------------------------------------------------------------------------------
 
+a = 456 # Integer
+b = 45.6 # Float
+c = 45j+6 # Complex
+s = 4.56e-3 # Scientific notation
+print(s)
+
+z = a+b*c # You can operate normally between types
+print(z)
+
+# Changing types
+
+a_complex = complex(a)
+b_integer = int(b)
+# c_float = float(c) # This wont' work because complex numbers can't be shown as simple floats.
+
+print(a_complex, b_integer)
+
+# Random number 
+
+import random # You can import mid-code in Python, but it's not pretty
+
+rnd_num = random.randrange(-56,45) # Generates a random number within the given range
+print(rnd_num)
+
+# Sequences -----------------------------------------------------------------------------------------
+# Text ----------------------------------------------------------------------------------------------
+# Mapping -------------------------------------------------------------------------------------------
+# Sets ----------------------------------------------------------------------------------------------
+# Booleans ------------------------------------------------------------------------------------------
+# Binaries ------------------------------------------------------------------------------------------

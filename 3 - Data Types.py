793368f0aa053f26e44a97dc89ea5ba23ea2a4e3
GabrielScholl <boaventura063@gmail.com>
2022-04-11 15:06:40 -0300

    MidMaking

--- 3 - Data Types.py
+++ 3 - Data Types.py
@@ -22,7 +22,8 @@ b_integer = int(b) # The int() constructor will remove any decimal information f
 print(a_complex, b_integer)
 
 # Random number 
-import random # You can import mid-code in Python, but it's not pretty
+import random
+from xml.dom.pulldom import SAX2DOM # You can import mid-code in Python, but it's not pretty
 
 rnd_num = random.randrange(-56,45) # Generates a random number within the given range
 print(rnd_num)
@@ -74,8 +75,63 @@ print(txt.format('bananas', 5, 'apple pies', 3.7, True))
 statement = '{1} is bigger than {0}'
 print(statement.format(3, 4))
 
+# Escape characters
+escapes = '''The value \" is a double quote, \' inserts a quote mid-quotes,
+\n creates a new line, \t is a tabulation, \f submits a form,
+\b is a backspace, \\ inserts a backslash, 
+this will be overwritten \r is a carriage return,
+\ ooo makes an octal and \ xhh an hexadecimal.\n'''
+print(escapes)
+
+# String methods
+print('gabriel'.capitalize()) # First char gets upper cased
+print('GABrIEL'.casefold()) # Same as lower()
+print('GABrIEL'.lower()) # Lower cases whole string
+print('gaBRiel'.upper()) # Upper cases whole string
+print('Gabriel'.center(20)) # Prints on total 20 chars, with string in the middle
+print('Gabriel'.count('a')) # Occurencies in the string
+print('Gåbrõül'.encode(encoding='ascii',errors='xmlcharrefreplace')) # Encodes replacing the unknown char
+print('Gåbrõül'.encode(encoding='ascii',errors='namereplace')) # Encodes describing the strange char, more errors options
+print('Gabriel'.endswith('el')) # True or false
+print('Gabriel'.startswith('Gabe')) # True or false
+print('Ga\tbri\tel'.expandtabs(10)) # Expands the tabs
+print('Gabriel'.find('i')) # Returns position
+print('Gabriel'.index('G')) # Returns index, same as .find()
+print('Gabrielli'.rfind('i')) # Returns trailing position
+print('Gabriel G.'.rindex('G')) # Returns trailing index, same as .rfind()
+print('Gabriel {height:.2f}'.format(height=1.950)) # Has many options after the colon
+print('Gabriel'.isalnum()) # Checks if all alphanumeric
+print('Gabriel'.isalpha()) # Checks if all alphabetic
+print('Gabriel'.isdecimal()) # Checks if all decimal
+print('Gabriel'.isdigit()) # Checks if all digits
+print('Gabriel'.isidentifier()) # Checks if the string is an identifier
+print('Gabriel'.islower()) # Checks if all lower case
+print('Gabriel'.isnumeric()) # Checks if all numeric
+print('Gabriel'.isprintable()) # Checks if all printable
+print('Gabriel'.isspace()) # Checks if all spaces
+print('Gabriel'.istitle()) # Checks if follows rules of a title - all words start and only start Uppercase
+print('gabriel'.title()) # Makes it follow rules of a title - all words start and only start Uppercase
+print('Gabriel'.isupper()) # Checks if all upper case
+print('+'.join(("Carne","Queijo","Bacon"))) # Joins items of iterable gluing with string
+print('Gabriel'.ljust(20)) # Returns left justified (by adding spaces right)
+print('Gabriel'.rjust(20)) # Returns right justified (by adding spaces left)
+print('Gabriel'.lstrip("G")) # Strips leading chars
+print('Gabriel'.rstrip("l")) # Strips trailing chars
+print('Gabriel'.strip()) # Strips leading and trailing chars (space is default)
+table = 'Gabriel'.maketrans("G","J") # Defines translation
+print('Gabriel'.translate(table)) # Translates
+print('Gabriel is tall and handsome'.partition("is")) # Divides in 3: pre match, match, and post
+print('Gabriel is tall and is handsome'.rpartition("is")) # Divides in 3: pre last match, last match, and post
+print('Gabriel'.replace("a","4").replace("i","1").replace("e","3").replace("G","6").replace("b","9").replace("r","8").replace("l","7"))
+print('Gabriel'.split("r")) # Split in char
+print('Gabriellerson'.rsplit("r", 1)) # Split in last n char occurrencies
+print('Gabriel\nBruno'.splitlines()) # Split lines
+print('Gabriel'.swapcase()) # Swaps the cases
+print('59.99'.zfill(5)) # Fills the start with zeroes until parameter length obtained
+print('9.50'.zfill(5))
+
+# Booleans ------------------------------------------------------------------------------------------
 # Sequences -----------------------------------------------------------------------------------------
 # Mapping -------------------------------------------------------------------------------------------
 # Sets ----------------------------------------------------------------------------------------------
-# Booleans ------------------------------------------------------------------------------------------
 # Binaries ------------------------------------------------------------------------------------------

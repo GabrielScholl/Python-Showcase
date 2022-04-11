fcb3845f62a5711afedcae02647eaa6daf153d53
Gabriel B. Scholl <48660276+GabrielScholl@users.noreply.github.com>
2022-04-11 08:54:12 -0300

    Adding file 2

---
+++ 2 - Variables.py
@@ -0,0 +1,18 @@
+# This file will cover variables
+
+x = 5 # x receives 5
+print(type(x)) # x is an int
+x = 'text' # x is now a string
+
+# Casting is changing the value type. Here we'll assign it to a variable
+
+a = str(5) # a is now "5" as in the string with those contents
+b = int(5.1) # b receives 5, an int
+c = float(5) # c's value is 5.0, a float
+
+# Capital letters
+
+A = "I'm capitalized" # Both constructions are correct and different variables.
+a = 'I\'m not' # Single or double quotes are the same thing
+
+print(a)

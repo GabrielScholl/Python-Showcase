896f893aa4e0f002f140f8311798504f7bd91b76
GabrielScholl <boaventura063@gmail.com>
2022-04-11 10:45:40 -0300

    Add

--- 3 - Data Types.py
+++ 3 - Data Types.py
@@ -1,1 +1,7 @@
 # This file will cover data types in Python
+# In Python, data can be one of 7 types:
+# Text, numbers, sequences, mapping, sets, boolean or binary.
+# Each one has different uses and constructions, as we'll see below.
+
+# Numbers
+

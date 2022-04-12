110bbe97cd6f5d518967a84fc6be8f375b6d3829
GabrielScholl <boaventura063@gmail.com>
2022-04-12 08:08:19 -0300

    Add file 4

--- 4 - Operators.py
+++ 4 - Operators.py
@@ -32,16 +32,53 @@ x //= 2; print(x)
 x **= 9; print(x)
 x = int(x)
 
-# The following are BITWISE operations between operands and assign to first operand.
-x &= 2;  print(x) # AND
-x |= 2;  print(x) # OR
-x ^= 2;  print(x) # xOR
-x >>= 2; print(x) # Bit Shift Right
-x <<= 2; print(x) # Bit Shift Left
+# The following are BITWISE operations between operands and then assign to first operand.
+x &= y;  print(x) # AND
+x |= y;  print(x) # OR
+x ^= y;  print(x) # xOR
+x >>= y; print(x) # Bit Shift Right
+x <<= y; print(x) # Bit Shift Left
 
 
 # Comparison -----------------------------------------------------------------------------------------
+
+
+x == y
+x != y
+x > y
+x < y
+x >= y
+x <= y
+
+
 # Logical --------------------------------------------------------------------------------------------
+
+
+x and y
+x or y
+not y
+
+
 # Identity -------------------------------------------------------------------------------------------
+
+
+x is y # True if both are the same object
+x is not y # True if not the same object
+
+
 # Membership -----------------------------------------------------------------------------------------
+
+
+x in y # True if the sequence is present in object
+x not in y # True if the sequence is NOT present in object
+
+
 # Bitwise --------------------------------------------------------------------------------------------
+
+
+x & y # AND - Sets both to 1 if both are 1
+x | y # OR - Sets both to 1 if either is 1
+x ^ y # xOR - Sets both to 1 if only one is 1
+~ y # NOT - Inverts all the bits
+x << y # Zero fill left shift
+x >> y # Signed right shift - Shifts content right, fills left with left digit
